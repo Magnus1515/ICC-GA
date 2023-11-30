ab839b2a32e0aaf4774a6426ce38c9b27c50fe48
David Roldan <82829595+Magnus1515@users.noreply.github.com>
2023-11-30 14:23:57 -0800

    Final version of ACO algorithm

--- Ejercicios/Final_Project/main.py
+++ Ejercicios/Final_Project/main.py
@@ -23,15 +23,15 @@ knapsack_capacity = 750
 
 pesos = [70, 73, 77, 80, 82, 87, 90, 94, 98, 106, 110, 113, 115, 118, 120]
 
-valores = [135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240]
+profits = [135, 139, 149, 150, 156, 163, 173, 184, 192, 201, 210, 214, 221, 229, 240]
 
-tao = 1.0
-ant_k = 0
-zj = 0
+p = 1.0
 
-# Variable feromonas 
+numeber_ants = 10
+n_runs = 100
+number_list = list(range(1, n_runs + 1))
 
-hormigas = []
+
 
 def random_poblation(cromosomas, genes):
     resultado = []
@@ -61,7 +61,6 @@ def Pj(valores,tao):
     return cumulative_sum_list
 
 
-
 def random_probabilidades(cantidad):
     lista = []
     for x in range(cantidad):
@@ -71,18 +70,21 @@ def random_probabilidades(cantidad):
     return lista
 
 
-#la cantidad de objetos va a depender del peso de la mochila 
-#la cantidad de soluciones que tengan no puede ser mayor que el peso de la mochila
+def print_poblation_line(poblacion):
+    for cromosoma in poblacion:
+        print(cromosoma)
 
-def ants_neighborhood(num_ants, num_obj):
 
-    hormigas = []
-    for _ in range(num_ants):
-        objetos_seleccionados = [random.choice([0, 1]) for _ in range(num_obj)]
-        capacidad_restante = calcular_capacidad_restante(objetos_seleccionados)
-        feromona = 1.0  # Puedes ajustar esto según sea necesario
-        hormigas.append(objetos_seleccionados)
-    return hormigas
+
+# def ants_neighborhood(num_ants, num_obj):
+
+#     hormigas = []
+#     for _ in range(num_ants):
+#         objetos_seleccionados = [random.choice([0, 1]) for _ in range(num_obj)]
+#         capacidad_restante = calcular_capacidad_restante(objetos_seleccionados)
+#         feromona = 1.0  # Puedes ajustar esto según sea necesario
+#         hormigas.append(objetos_seleccionados)
+#     return hormigas
 
 
 
@@ -92,29 +94,60 @@ def ants_neighborhood(num_ants, num_obj):
     #Si no ha trabajo todavia, existe
     #while ant_k == 0:
 
-def update_pheromone(ants, evaporation_rate):
 
-    while knapsack_capacity >= 0:
-        #seleccionamos un Objeto
-        random_prob_list = random_probabilidades(15)
-        cumulative_list_valores = Pj(valores,tao)
+#la cantidad de objetos va a depender del peso de la mochila 
+#la cantidad de soluciones que tengan no puede ser mayor que el peso de la mochila
 
-        for numero_aleatorio in random_prob_list:
-            for i,acumulado in enumerate(cumulative_list_valores):
-                if numero_aleatorio <= acumulado:
-                    selected_elem.append(pesos[i])
-                    print(pesos[i])
-                    knapsack_capacity -= pesos[i]
-                    zj += valores[i]
+def ant_colony(ants, knapsack_capacity,profits,p):
+
+    ants_colony_w_list = []
+    ants_colony_p_list = []
+    
+    
+    #seleccionamos un Objeto
+    for _ in range(ants):
+        ant_unit_w_list = []
+        ant_unit_p_list =[]
+        cumulative_list_valores = Pj(profits,p)
+        capacidad_mochila = knapsack_capacity
+        for i,acumulado in enumerate(cumulative_list_valores):
+            # while(sum(hormiga_list) > knapsack_capacity):
+            numero_aleatorio = random_probabilidades(1)
+            if numero_aleatorio[0] <= acumulado:
+                if pesos[i] <= capacidad_mochila:
+                    ant_unit_w_list.append(pesos[i])
+                    ant_unit_p_list.append(profits[i])
+                    capacidad_mochila -= pesos[i]
+             
+        #Add the whole ant unit list to save that list
+        ants_colony_w_list.append(sum(ant_unit_w_list)) 
+        ants_colony_p_list.append(sum(ant_unit_p_list)) 
+
+    return max(ants_colony_p_list)
 
 
 # Una que crea las hormigas los obejtos, la capacidad, la feromona -> y reparar
 # Una que me actualiza las feromonas
 # Una que te selecciona los objetos 
 # Funcion principal
-   
-                    
+
+best_solution_after_runs = []
+
+for _ in range(n_runs):
+
+    
+    temp_solution = ant_colony(10,knapsack_capacity,profits,p)    
+    best_solution_after_runs.append(temp_solution)
 
 
 
- 
+data = list(zip(number_list,best_solution_after_runs))
+
+table = tabulate(data, headers=["Runs","Best Chromosome", "Best Fitness"], tablefmt="fancy_grid")
+
+
+max_solution_after_runs = max(best_solution_after_runs)
+
+print(table)
+
+print(f"Best Solutions of {n_runs}: {max_solution_after_runs}")
--- Ejercicios/Parcial_2/main.py
+++
@@ -1,6 +0,0 @@
-num_steps = 100000000
-
-step = num_steps/4
-
-
-print(step)
